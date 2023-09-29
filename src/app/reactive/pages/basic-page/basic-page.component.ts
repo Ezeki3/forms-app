c0182d3f60ad3c047f6fde992b976cc5366e15d8
Ezequiel Lino <sonisombra@gmail.com>
2023-09-29 17:54:36 -0600

    formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicPageComponent implements OnInit {
 
-  constructor() { }
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(''),
+  //   inStorage: new FormControl(''),
+  // })
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name:[''],
+    price:[0],
+    inStorage:[0]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  onSave():void {
+    console.log(this.myForm.value)
   }
 
 }
