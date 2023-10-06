c607c172e4c81334a0e919323879dedfc8f33bf6
Ezequiel Lino <sonisombra@gmail.com>
2023-10-06 11:11:59 -0600

    validators functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validator';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -11,7 +12,7 @@ export class RegisterPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required] ],
     email: ['', [Validators.required] ],
-    username: ['', [Validators.required] ],
+    username: ['', [Validators.required, cantBeStrider] ],
     password: ['', [Validators.required, Validators.minLength(6)] ],
     password2: ['', [Validators.required] ],
   })
@@ -25,7 +26,7 @@ export class RegisterPageComponent implements OnInit {
 
   isValidField( field: string){
     //TODO: Obtener validacion desde un servicio
-    
+
   }
 
   onSubmit(){
